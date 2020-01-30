aae96d236cb30f9a579df90dec61f9230939bc97
Jiancong <jiancong.zheng@rwth-aachen.de>
2020-01-30 23:44:14 +0100

    # add buttons of initialization and relative rotations in GUI

--- ChessboardDetectorGUI/GUI/GUI.h
+++ ChessboardDetectorGUI/GUI/GUI.h
@@ -2,10 +2,14 @@
 
 #include <QtWidgets/QMainWindow>
 #include <QLabel>
+#include <QMessageBox>
 #include <QPushButton>
-#include "ui_GUI.h"
-#include "MoveComputer.h"
 #include <atlstr.h>
+#include "ui_GUI.h"
+#include "Scanner.h"
+#include "MoveComputer.h"
+#include <sstream>
+#include <iostream>
 
 class GUI : public QMainWindow
 {
@@ -15,7 +19,17 @@ public:
 	~GUI() {};
 private:
 	Ui::GUIClass ui;
+	QString warning;
+	MoveComputer mover;
+	QString displacement;
+	double dispValue;
+	std::stringstream stream;
+	void _labelDisplayMat(QLabel* label);
+	bool _isNumber(std::string str);
 private slots:
 	void initializationSlot();
-	void moveSlot();
+	void movePositiveXSlot();
+	void moveNegativeXSlot();
+	void movePositiveYSlot();
+	void moveNegativeYSlot();
 };
