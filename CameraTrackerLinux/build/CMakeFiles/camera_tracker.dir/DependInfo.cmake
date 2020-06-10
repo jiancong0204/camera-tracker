6ff28d27e866f2e65e3b671a881a85756433f27d
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-06-10 16:52:38 +0200

    delete /build

---
+++ CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/main.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/main.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/BaslerGigECamera.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/BaslerGigECamera.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/Chessboard.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/Chessboard.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/ChessboardDetector.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/ChessboardDetector.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/Conex.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/Conex.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/GenericCamera.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/GenericCamera.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/RotationStage.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/RotationStage.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/Scanner.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/Scanner.cpp.o"
+  "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/src/TimedFrame.cpp" "/home/bdn_jz/bdn_jz/camera-tracker-cpp/CameraTrackerLinux/build/CMakeFiles/camera_tracker.dir/src/TimedFrame.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/opencv4"
+  "/opt/pylon5/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
