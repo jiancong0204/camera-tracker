a028635b30984403d8d5f6f2c696fd3ece87d1ed
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-06-17 13:38:42 +0200

    modify Tracker class

--- CameraTrackerLinux/src/Tracker.cpp
+++ CameraTrackerLinux/src/Tracker.cpp
@@ -1,10 +1,14 @@
 #include "Tracker.h"
 
-void Tracker::startTracking()
+void Tracker::tracking()
 {
+    // Initialize the rotation stage
     this->rs.setElevationPort();
+    this->rs.setAzimuthPort();
     this->rs.initializeElevation();
     this->rs.initializeAzimuth();
+    
+    // Initialize the Gige camera
     BaslerGigECamera camera;
     std::vector<std::string> cameraList = camera.listAvailableDevices();
 	std::string name = cameraList[0];
@@ -13,6 +17,42 @@ void Tracker::startTracking()
     // TODO: while loop
 	    cv::Mat img = camera.getFrame();
         _computeRotationAngles(img);
-        this->rs.relativeMove("USB0", 15);
-        this->rs.absoluteMove("USB1", 15);
+        this->rs.relativeMoveElevation(this->elevationAngle);
+        this->rs.relativeMoveAzimuth(this->azimuthAngle);
+
+    camera.detach();
+}
+
+float Tracker::getAzimuth()
+{
+    return this->azimuthAngle;
+}
+
+float Tracker::getElevation()
+{
+    return this->elevationAngle;
+}
+
+void Tracker::_computeRotationAngles(cv::Mat img)
+{
+    img.convertTo(img, CV_8U);
+    Chessboard chessboard(9, 7, 20);
+	ChessboardDetector detector(chessboard, img);
+	ChessboardDetectorResult detectionResullt = detector.getResult();
+
+    if (detectionResullt.success)
+    {
+        printf("Chessboard detection is successful!\n");
+        PoseEstimation pose = PoseEstimation(chessboard.getGrid(), detectionResullt.scale, chessboard, detectionResullt.corners);
+	    cv::Mat raux = pose.getRvecs();
+	    cv::Mat taux = pose.getTvecs();
+        this->elevationAngle = atan(taux.at<double>(1, 0) / taux.at<double>(2, 0)) * (180 / atan(1) / 4) * -1;
+		this->azimuthAngle = atan(taux.at<double>(0, 0) / taux.at<double>(2, 0)) * (180 / atan(1) / 4);
+    }
+    else
+    {
+        printf("Chessboard detection is failed!\n");
+        this->elevationAngle = 0;
+        this->azimuthAngle = 0;
+    }
 }
