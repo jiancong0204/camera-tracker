348c36542aa43f2c7d575a97b00589e486781c3a
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-06-17 11:46:51 +0200

    update main.cpp

--- CameraTrackerLinux/main.cpp
+++ CameraTrackerLinux/main.cpp
@@ -7,12 +7,12 @@
 
 int main()
 {
-    // BaslerGigECamera camera;
-    // std::vector<std::string> cameraList = camera.listAvailableDevices();
-	// std::string name = cameraList[0];
-	// camera.initialize(name);
-	// cv::Mat img = camera.getFrame();
-    cv::Mat img = cv::imread("../Images/01.jpg",cv::IMREAD_GRAYSCALE);
+    BaslerGigECamera camera;
+    std::vector<std::string> cameraList = camera.listAvailableDevices();
+	std::string name = cameraList[0];
+	camera.initialize(name);
+	cv::Mat img = camera.getFrame();
+    // cv::Mat img = cv::imread("../Images/01.jpg",cv::IMREAD_GRAYSCALE);
     img.convertTo(img, CV_8U);
     Chessboard chessboard(9, 7, 20);
 	ChessboardDetector detector(chessboard, img);
@@ -21,45 +21,45 @@ int main()
     if (detectionResullt.success)
     {
         printf("Chessboard detection is successful!\n");
+        PoseEstimation pose = PoseEstimation(chessboard.getGrid(), detectionResullt.scale, chessboard, detectionResullt.corners);
+	    cv::Mat raux = pose.getRvecs();
+	    cv::Mat taux = pose.getTvecs();
+        for (int i = 0; i < 3; i++)
+        {    
+            std::cout << raux.at<double>(i, 0) << std::endl;
+        }
+
+        BarcodeScanner barcode = BarcodeScanner(detectionResullt);
+        QrcodeScanner qrcode = QrcodeScanner(detectionResullt);
+        CodeInfo qr = qrcode.getQrCode();
+        CodeInfo bar = barcode.getBarCode();
+        if (qr.codeType == "null")
+        {
+            std::cout << qr.codeData << std::endl;
+        }
+        else 
+        {
+            std::cout << "Type: " + qr.codeType + "; " + "Data: " + qr.codeData + ";" << std::endl;
+        }
+        
+        if (bar.codeType == "null")
+        {
+            std::cout << bar.codeData << std::endl;
+        }
+        else 
+        {
+            std::cout << "Type: " + bar.codeType + "; " + "Data: " + bar.codeData + ";" << std::endl;
+        }
     }
     else
     {
         printf("Chessboard detection is failed!\n");
-        return -1;
+        // return -1;
     }
 
-    BarcodeScanner barcode = BarcodeScanner(detectionResullt);
-	QrcodeScanner qrcode = QrcodeScanner(detectionResullt);
-	CodeInfo qr = qrcode.getQrCode();
-	CodeInfo bar = barcode.getBarCode();
-    if (qr.codeType == "null")
-    {
-        std::cout << qr.codeData << std::endl;
-    }
-    else 
-    {
-        std::cout << "Type: " + qr.codeType + "; " + "Data: " + qr.codeData + ";" << std::endl;
-    }
-    
-    if (bar.codeType == "null")
-    {
-        std::cout << bar.codeData << std::endl;
-    }
-    else 
-    {
-        std::cout << "Type: " + bar.codeType + "; " + "Data: " + bar.codeData + ";" << std::endl;
-    }
-
-    PoseEstimation pose = PoseEstimation(chessboard.getGrid(), detectionResullt.scale, chessboard, detectionResullt.corners);
-	cv::Mat raux = pose.getRvecs();
-	cv::Mat taux = pose.getTvecs();
-    for (int i = 0; i < 3; i++)
-    {    
-        std::cout << raux.at<double>(i, 0) << std::endl;
-    }
-    // cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE ); // Create a window for display
-    // cv::imshow( "Display window", img );
-    // cv::waitKey(0); // Wait for a keystroke in the window
+    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE ); // Create a window for display
+    cv::imshow( "Display window", img );
+    cv::waitKey(0); // Wait for a keystroke in the window
     // RotationStage rs;
     // rs.initialize("USB0");
     // rs.initialize("USB1");
