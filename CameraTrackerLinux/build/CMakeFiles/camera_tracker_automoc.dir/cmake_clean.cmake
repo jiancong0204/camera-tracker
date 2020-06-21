1c07c09e83e98c167a8b4467348431071b819d84
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-06-21 23:45:25 +0200

    update .gitignore

---
+++ CameraTrackerLinux/build/CMakeFiles/camera_tracker_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "camera_tracker_automoc.cpp"
+  "CMakeFiles/camera_tracker_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/camera_tracker_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
