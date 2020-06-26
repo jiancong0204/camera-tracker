e69b67952080edd99bdb94213bc3fa7d4bea357c
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-06-26 10:54:26 +0200

    add slots for moving camera

--- CameraTrackerLinux/include/TrackerGUI.h
+++ CameraTrackerLinux/include/TrackerGUI.h
@@ -18,6 +18,56 @@ public:
 
 private:
     Ui::TrackerGUI *ui;
+
+private slots:
+
+    /**
+     * @brief Slot for initialization
+     * 
+     */
+    void initializationSlot();
+
+    /**
+     * @brief  Slot for moving in positive x
+     * 
+     */
+	void movePositiveXSlot();
+
+    /**
+     * @brief Slot for moving in negative x
+     * 
+     */
+	void moveNegativeXSlot();
+
+    /**
+     * @brief Slot for moving in positive y
+     * 
+     */
+	void movePositiveYSlot();
+
+    /**
+     * @brief Slot for moving in negative y
+     * 
+     */
+	void moveNegativeYSlot();
+
+    /**
+     * @brief Slot for absolute moving in x-direction
+     * 
+     */
+	void gotoXSlot();
+
+    /**
+     * @brief Slot for absolute moving in y-direction
+     * 
+     */
+	void gotoYSlot();
+
+    /**
+     * @brief Slot for pose estimation
+     * 
+     */
+    void cameraPoseEstimationSlot();
 };
 
 #endif // TRACKERGUI_H
