5f011c6118818917ced486c04413079c03776d54
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-06-29 12:58:51 +0200

    update main.cpp

--- CameraTrackerLinux/main.cpp
+++ CameraTrackerLinux/main.cpp
@@ -4,8 +4,9 @@ int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     TrackerGUI w;
+    w.setWindowTitle("Camera Tracker");
+	QIcon icon("../Images/target.png");
+	w.setWindowIcon(icon);
     w.show();
     return a.exec();
-
-    return 0;
 }
